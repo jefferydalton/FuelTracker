a7544f5d653ae45fc49e69a0fb610a1d017b96d6
Jeff Dalton <jefferydalton@hotmail.com>
2014-08-03 09:26:46 -0400

    Added StationId Value Type and moved to Model Namespace

---
+++ FuelTracker/Domain/Model/Station/StationId.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FuelTracker.Extensions;
+
+namespace FuelTracker.Domain.Model.Station
+{
+    public class StationId
+    {
+
+        private StationId() { }
+
+        internal StationId(Int32 id)
+        {
+            this.Id = id;
+        }
+
+        internal StationId(StationId id)
+        {
+            this.Id = id.Id;
+        }
+
+        public Int32 Id
+        {
+            get
+            {
+                return id;
+            }
+            private set
+            {
+                value.AssertValueNotZero("The Id must be provided.");
+                this.id = value;
+            }
+        }
+        private Int32 id;
+    }
+}
---
+++ FuelTracker/Extensions/ValueObjectRules.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuelTracker.Extensions
+{
+    public static class ValueObjectRules
+    {
+        public static void AssertValueNotZero(this Int32 val, string  message)
+        {
+            if (val == 0)
+            {
+                throw new ArgumentException(message);
+            }
+        }
+    }
+}
